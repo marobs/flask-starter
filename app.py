19c7eae2b860eb697cd9b1d86ae7e0f290c22333
Michael Roberts <marobs@umich.edu>
2017-06-23 11:27:22 -0400

    Basic flask files

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+import controllers
+import helpers
+
+# Initialize Flask app with the template folder address
+app = Flask(__name__, template_folder='templates')
+
+# Register the controllers
+app.register_blueprint(controllers.main)
+app.register_blueprint(controllers.genre)
+app.secret_key = helpers.getFlaskSecret()
+
+# Listen on external IPs
+# For us, listen to port 3000 so you can just run 'python app.py' to start the server
+if __name__ == '__main__':
+    # listen on external IPs
+    app.run(host='0.0.0.0', port=3000, debug=True)
